117e18fa7995b889fcd948dc62a2d7fcb0219f28
Paris <parismollochristondis@gmail.com>
2019-12-09 16:11:06 +0100

    add tests

--- model_test.py
+++ model_test.py
@@ -12,3 +12,25 @@ assert model.spam_messages == 1
 assert model.ham_messages == 2
 assert model.token_spam_counts == {"spam": 1, "rules": 1}
 assert model.token_ham_counts == {"ham": 2, "rules": 1, "hello":1}
+
+text = "hello spam"
+
+probs_if_spam = [
+    (1 + 0.5) / (1 + 2 * 0.5),      # "spam"  (present)
+    1 - (0 + 0.5) / (1 + 2 * 0.5),  # "ham"   (not present)
+    1 - (1 + 0.5) / (1 + 2 * 0.5),  # "rules" (not present)
+    (0 + 0.5) / (1 + 2 * 0.5)       # "hello" (present)
+]
+
+probs_if_ham = [
+    (0 + 0.5) / (2 + 2 * 0.5),      # "spam"  (present)
+    1 - (2 + 0.5) / (2 + 2 * 0.5),  # "ham"   (not present)
+    1 - (1 + 0.5) / (2 + 2 * 0.5),  # "rules" (not present)
+    (1 + 0.5) / (2 + 2 * 0.5),      # "hello" (present)
+]
+
+p_if_spam = math.exp(sum(math.log(p) for p in probs_if_spam))
+p_if_ham = math.exp(sum(math.log(p) for p in probs_if_ham))
+
+# Should be about 0.83
+assert model.predict(text) == p_if_spam / (p_if_spam + p_if_ham)
